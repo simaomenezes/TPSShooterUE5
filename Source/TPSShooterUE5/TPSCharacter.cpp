f22e8fec60eba2184637b7943d61527da6fdce19
Simão Neto <simaomenezesneto@gmail.com>
2024-12-01 20:34:17 +0000

    feat: UE_LOG Format Specifiers

--- Source/TPSShooterUE5/TPSCharacter.cpp
+++ Source/TPSShooterUE5/TPSCharacter.cpp
@@ -20,6 +20,24 @@ void ATPSCharacter::BeginPlay()
 
 	int myInt{ 42 };
 	UE_LOG(LogTemp, Warning, TEXT("int myInt: %d"), myInt);
+
+	float myFloat{ 3.14159f};
+	UE_LOG(LogTemp, Warning, TEXT("float myFloat: %f"), myFloat);
+
+	double myDouble{ 3.14159 };
+	UE_LOG(LogTemp, Warning, TEXT("double myDouble: %lf"), myDouble);
+
+	TCHAR myChar{ 'A' };
+	UE_LOG(LogTemp, Warning, TEXT("char myChar: %c"), myChar);
+
+	wchar_t myWChar{ L'A' };
+	UE_LOG(LogTemp, Warning, TEXT("wchar_t myWChar: %%lc"), myWChar);
+
+	bool myBool{ true };
+	UE_LOG(LogTemp, Warning, TEXT("bool myBool: %d"), myBool);
+
+	UE_LOG(LogTemp, Warning, TEXT("int: %d, float: %f, bool: %d"), myInt, myFloat, myBool);
+
 	
 }
 
